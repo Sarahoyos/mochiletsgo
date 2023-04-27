e66bb8a1419b4ab95edc8b70109ca0766e302330
sara <75278809+Sarahoyos@users.noreply.github.com>
2023-04-27 17:05:37 -0500

    Add files via upload

---
+++ conectiondb.py
@@ -0,0 +1,6 @@
+import pyodbc
+server = 'localhost'
+db = 'venture'
+user = 'sarah'
+password = 'w^zXz2@0lZPi'
+
---
+++ manage.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+#enlanzar las pagina de home con las paginas auxliares
+def main():
+    os.environ.setdefault('DJANGO_SETTING_MODULE','base_project.settings') # una variable de entorno que me enlace paginas 
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            'General error' # cuando le dio click una ruta me sale este error , tenemos que ver que sucede con la validacion
+        ) from exc
+    execute_from_command_line(sys.argv)
+    
+if __name__== '_main_': # si entro check is correcto si no muestreme el error , y redirrecionar a home 
+    main()
