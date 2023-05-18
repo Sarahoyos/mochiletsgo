57e12bf4d872c7cd841e91d232f1639521cb1ac1
Sara Arango <75278809+Sarahoyos@users.noreply.github.com>
2023-05-17 22:20:04 -0500

    changes

--- html/server.js
+++ html/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5800;
 
 express()
-  .use(express.static(path.join(__dirname, '../../dist/apps/desplegue_pagina')))
+  .use(express.static(path.join(__dirname, '../../dist/apps/homepage')))
   .get('*', (_, res) => {
     res.sendFile('SignUpAD.html', {
-      root: path.join(__dirname, '../../dist/apps/desplegue_pagin'),
+      root: path.join(__dirname, '../../dist/apps/homepage'),
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
